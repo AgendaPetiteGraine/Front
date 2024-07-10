4fce8257ca691324ee36e17159b028ce2c6831e5
sophie <scboyer.dev@gmail.com>
2024-07-10 21:38:44 +0200

    without northflant img in .mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "maps.googleapis.com",
-      "site--petitegraine--xj5ljztnmr2k.code.run/",
-    ],
+    domains: ["res.cloudinary.com", "maps.googleapis.com"],
   },
 };
 
