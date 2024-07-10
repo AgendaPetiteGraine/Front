6eea073c625f6cbc39b4a5235be9181e4eb6495a
sophie <scboyer.dev@gmail.com>
2024-07-10 21:40:28 +0200

    another test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "res.cloudinary.com",
-      "maps.googleapis.com",
-      "site--petitegraine--xj5ljztnmr2k.code.run",
-    ], // Ajoutez ici les domaines externes si vous utilisez des images hébergées sur d'autres sites
-  },
 };
