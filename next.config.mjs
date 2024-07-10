5828f83b72aeb0b64edbb490878844963a62dbeb
sophie <scboyer.dev@gmail.com>
2024-07-10 21:32:44 +0200

    adding nortflank img

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "maps.googleapis.com"],
+    domains: [
+      "res.cloudinary.com",
+      "maps.googleapis.com",
+      "site--petitegraine--xj5ljztnmr2k.code.run/",
+    ],
   },
 };
 
